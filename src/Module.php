635c2182258a7662683720e7f31dade785b4cbac
Gian Nunes <gian@btitecnologia.com>
2016-12-24 14:52:36 -0200

    refatorando

--- src/Module.php
+++ src/Module.php
@@ -7,11 +7,6 @@ class Module {
     public function getConfig() {
         return require __DIR__ . '/config.php';
     }
-
-    public function getEventGlobal() {
-        return require __DIR__ . '/event.global.php';
-    }
-
     public function getDir() {
         return __DIR__;
     }
